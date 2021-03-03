cce47d8a3f9bbf8c95ad205357e7cbf0081d1cfc
Andrew Sanville <amsanville@gmail.com>
2021-03-03 08:39:57 -0800

    Added buttons that allow manipulation of the graph

--- js/main.js
+++ js/main.js
@@ -1,96 +1,125 @@
 // Global variables with default values for the page
 let base = 'EUR';
-let countries = ['EUR', 'USD', 'GBP', 'CAD', 'AUD'];
+let initCountries = ['EUR', 'USD', 'AUD', 'CAD'];
+let currData = {};
 
-function render(){
-    // Renders the page
-    console.log('Rendering page');
-
-    // Fetch the data specified by input
-    console.log('Fetching the data');
-    // Build the fetch url
-    let url = 'https://api.exchangeratesapi.io/latest?base=' + base;
-
+function initialize(){
+    // Run the initial render to populate the button bar and bar graph
+    let url = 'https://api.exchangeratesapi.io/latest';
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            console.log('Building the buttons')
+            // Cache the current data
+            currData = data.rates;
+            base = data.base;
+            currData[base] = 1.0;
 
-            // Render the bar graph
-            console.log('Building the bar graph')
-            let bars = document.querySelector('#graph');           
+            // Process the initial API call into the buttons at the top
+            let selector = document.createElement('select');
+            selector.setAttribute('id', 'selector');
+            selector.addEventListener('change', newSelection);
 
-            // Get the maximum value from the currency exchange
-            let min = Number.MAX_SAFE_INTEGER;
-            for(const [key, value] of Object.entries(data.rates)){
-                if(min > value){
-                    min = value;
+            let toggles = document.createElement('div');
+            toggles.classList.add('DataVis-title-buttonbar');
+
+            // Make a list of all the countries to add
+            let allCountries = Object.keys(currData);
+            allCountries.sort();
+
+            // Create a button and drop down option for each country
+            for(let country of allCountries){
+                // Create a new option for each 
+                newOption = document.createElement('option')
+                newOption.textContent = country;
+                selector.appendChild(newOption);
+
+                // Create a toggle button for each country found in the exchange data base
+                newToggle = document.createElement('button');
+                newToggle.textContent = country;
+                newToggle.classList.add('DataVis-title-buttonbar-button');
+                newToggle.addEventListener('click', function(){
+                    toggleCountry(this)
+                });
+                // Toggle the default settings
+                if(initCountries.includes(country)){
+                    newToggle.classList.add('DataVis-title-buttonbar-button--toggle');
                 }
+                toggles.appendChild(newToggle);
             }
+            // Make sure the base is chosen
+            selector.value = data.base;
+
+            // Add to the title bar with labels
+            let titleBar = document.querySelector('.DataVis-title');
             
-            // Version 1 - create new elements and append
-            // Remove all children
-            while(bars.firstChild){
-                bars.removeChild(bars.firstChild)
-            }
-            
-            // Special add for the base country
-            baseIndex = countries.indexOf(base)
-            if(baseIndex > -1){
-                // Splice it out of the array
-                countries.splice(baseIndex, 1);
+            let p1 = document.createElement('p');
+            p1.textContent = 'Choose base currency:';
+            titleBar.appendChild(p1);
+            titleBar.appendChild(selector);
 
-                // Add new div for it
-                let newBar = document.createElement('div');
-                newBar.className = 'DataVis-content-bar';
-                newBar.style.height = min * 100 + '%';
-                newBar.textContent = base;
-                newBar.onclick = function(){ alert(base + ' to ' + base + ': 1 to 1'); }
-                bars.appendChild(newBar);
-            }
+            let p2 = document.createElement('p');
+            p2.textContent = 'Toggle countries to display:';
+            titleBar.appendChild(p2);
+            titleBar.appendChild(toggles);
 
-            // Add children back in
-            for(let country of countries){                
-                // Create an element to hold the data
-                let newBar = document.createElement('div');
-                newBar.className = 'DataVis-content-bar';
-                newBar.style.height = (min / data.rates[country]) * 100 + '%';
-                newBar.textContent = country;
-                newBar.onclick = function(){ alert(key + ' to ' + base + ': ' + value + ' to 1'); }
-                bars.appendChild(newBar);
-            }
-            
-            /*
-            // Version 2 - add HTML with backtick
-            // Reset inner HTML
-            bars.innerHTML = '';
+            // Finally, build the graph
+            makeGraph();
+        });    
+}
 
-            // Special add for the base country
-            baseIndex = countries.indexOf(base)
-            if(baseIndex > -1){
-                // Splice it out of the array
-                countries.splice(baseIndex, 1);
+function makeGraph(){
+    // Builds the graph based on the data
+ 
+    // Get the toggled buttons
+    let toggled = document.querySelectorAll('.DataVis-title-buttonbar-button--toggle');
 
-                // Add new div for it
-                let height = min * 100;
-                bars.innerHTML += `<div class="DataVis-content-bar" OnClick="alert('${base} to ${base}: 1 to 1')" style="height: ${height}%">
-                    ${base}
-                </div>
-                `
-            }
+    // First find the minimum rate for all the countries
+    let min = Number.MAX_SAFE_INTEGER;
+    for(let currCountry of toggled){
+        let value = currData[currCountry.textContent];
+        if(min > value){
+            min = value
+        }
+    }
 
-            // Add each bar individually
-            for(let country of countries){
-                let value = data.rates[country];
-                let height = (min / value) * 100;
-                bars.innerHTML += `<div class="DataVis-content-bar" OnClick="alert('${country} to ${base}: ${value} to 1')" style="height: ${height}%">
-                    ${country}
-                </div>
-                `
-            }     
-            */       
+    // Get the bar graph area and clear the area
+    let bars = document.querySelector('#graph'); 
+    bars.innerHTML = '';
+
+    // Add children back in
+    for (let currCountry of toggled) {
+        let value = currData[currCountry.textContent];
+
+        // Create an element to hold the data
+        let newBar = document.createElement('div');
+        newBar.className = 'DataVis-content-bar';
+        newBar.style.height = (min / value) * 100 + '%';
+        newBar.textContent = currCountry.textContent;
+        newBar.onclick = function () { alert(currCountry.textContent + ' to ' + base + ': ' + value + ' to 1'); }
+        bars.appendChild(newBar);
+    }
+}
+
+function newSelection(){
+    // Change the base currency, so make a new API request and rebuild the graph
+    let selection = document.querySelector('select');
+    base = selection.value;
+    
+    // Fetch new data due to the change of the base
+    let url = 'https://api.exchangeratesapi.io/latest?base=' + base;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            currData = data.rates;
+            currData[base] = 1.0;
+            makeGraph();
         });
 }
 
-render();
+function toggleCountry(elem){
+    // Toggles the country and re-renders the page
+    elem.classList.toggle('DataVis-title-buttonbar-button--toggle');
+    makeGraph();
+}
+
+initialize();
